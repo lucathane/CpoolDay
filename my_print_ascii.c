5fe76cf4d463cb0091940796f37cc6ffe583b93f
lucathane <139331152+lucathane@users.noreply.github.com>
2023-07-12 09:53:24 +0000

    my_print_ascii

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -1,7 +1,8 @@
-#include "cobra.h"
+void print_char(char c) ;
 
 void my_print_ascii(void) {
     for (char ascii = '!' ; ascii != '~'+1; ascii++) {
-        printf("%c",ascii) ;
+        print_char(ascii) ;
     }
 }
+
