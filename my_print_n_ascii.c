2badc88cc52af0ebacd567ffe744bcdb1cb7698c
lucathane <139331152+lucathane@users.noreply.github.com>
2023-07-12 10:29:48 +0000

    print_n_ascii

---
+++ my_print_n_ascii.c
@@ -0,0 +1,17 @@
+void print_char(char c);
+void my_print_n_ascii(int howMany) {
+    int ascii=33 ;
+    int nb=howMany ;
+    nb+=ascii ;
+    if (nb <=32 || nb>= 128){
+        return ;
+    }
+    else{
+        for (ascii = '!' ; ascii != nb+1; ascii++) {
+        print_char(ascii) ;
+        }
+    } 
+
+}
+
+
